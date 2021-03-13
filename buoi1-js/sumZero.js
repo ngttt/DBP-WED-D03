8edb0b206ba83dafc3f1325ea9e8dccb1e49dc08
ngttt <1754050092tien@ou.edu.vn>
2021-03-13 20:26:31 +0700

    commit

--- buoi1-js/sumZero.js
+++ buoi1-js/sumZero.js
@@ -1,8 +1,10 @@
 function sumZero(array) {
   let start = 0;
   let end = array.length - 1;
+
   while (start < end) {
     let sum = array[start] + array[end];
+
     if (sum === 0) {
       return [array[start], array[end]];
     } else if (sum > 0) {
