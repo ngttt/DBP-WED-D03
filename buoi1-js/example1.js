4f5da79c3fea831d9af86b02a28bbfdafa2ab66c
ngttt <1754050092tien@ou.edu.vn>
2021-04-05 22:54:51 +0700

    jquery

--- buoi1-js/example1.js
+++
@@ -1,7 +0,0 @@
-let a1 = 1;
-let a2 = 1;
-console.log(a1 === a2);
-
-let obj1 = { a: 1 };
-let obj2 = { a: 2 };
-console.log(obj1 === obj2);
--- buoi1-js/example7.js
+++
@@ -1,8 +0,0 @@
-var numbers = [2, 5, 1, 4, 9, 10];
-
-function useFilter(array) {
-  var result = array.filter((number) => number % 2 == 0);
-  return result;
-}
-
-console.log(useFilter(numbers));
